c49c1c94dc8e18fcd94975a8c298cc5b3b0357ca
mponsa-meli <manuel.ponsa@mercadolibre.com>
2020-10-02 21:43:56 -0300

    Fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import { AddCar, Home, EditCar, ListCar} from './screens';
+import React, {useState} from 'react';
+import { AddCar, Home, EditCar, ListCar, Login} from './screens';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
+import { AuthContext } from './context/auth';
+import PrivateRoute from './PrivateRoute';
 
 function App() {
+  const existingTokens = JSON.parse(localStorage.getItem("tokens"));
+  const [authTokens, setAuthTokens] = useState(existingTokens);
+  
+  const setTokens = (data) => {
+    localStorage.setItem("tokens", JSON.stringify(data));
+    setAuthTokens(data);
+  }
+
   return (
+    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
     <Router>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/addcar" component={AddCar} />
-          <Route exact path="/editcar" component={EditCar} />
-          <Route exact path="/listcar" component={ ListCar } />
+          <Route path="/login" component={Login} />
+          <PrivateRoute exact path="/" component={Home} />
+          <PrivateRoute exact path="/addcar" component={AddCar} />
+          <PrivateRoute exact path="/editcar" component={EditCar} />
+          <PrivateRoute exact path="/listcar" component={ ListCar } />
         </Switch>
     </Router>
+    </AuthContext.Provider>
   );
 }
 
---
+++ src/screens/Login.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react'
+import { Logo, Input } from '../components'
+import { Button } from 'react-bootstrap'
+import axios from 'axios';
+import { Link, Redirect } from 'react-router-dom'
+import { useAuth } from "../context/auth";
+
+import "./Login.css"
+
+
+export default function Login() {
+    const [isLoggedIn, setLoggedIn] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const { setAuthTokens } = useAuth();
+
+    function postLogin() {
+        const headers = { 
+            'x-api-key': '38E562721ACB3769C4'
+        }
+        const data = {
+            username,
+            password
+        }
+        axios.post("https://ssoia.herokuapp.com/Login", {
+            headers,
+            data
+        }).then(result => {
+          if (result.status === 200) {
+            setAuthTokens(result.token);
+            setLoggedIn(true);
+          } else {
+            setIsError(true);
+          }
+        }).catch(e => {
+          setIsError(true);
+        });
+      }
+    
+      if (isLoggedIn) {
+        return <Redirect to="/" />;
+      }
+
+
+    return (
+        <div className="home" >
+            <Logo title="Rent-a-car" subTitle="Administrator App" />
+            <div className="form">
+                <Input 
+                    type="email" 
+                    className='input' 
+                    placeholder="email"
+                    value={username} 
+                    onChange={e => {
+                        setUsername(e.target.value);
+                    }}
+                    />
+                <Input 
+                    type="password"
+                    value={password} 
+                    className='input' 
+                    placeholder="password" 
+                    onChange={e => {
+                        setPassword(e.target.value);
+                    }}/>
+            </div>
+            <Link>
+                    <Button size="lg" onClick={postLogin}>
+                            Login
+                    </Button>
+            </Link>
+        </div>
+    )
+}
