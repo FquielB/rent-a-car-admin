dc4b3312ce5f38de9fb8be98c88b93903f4d6910
FquielB <bacigalupofacundo@gmail.com>
2020-10-02 22:02:15 -0300

    Styles upgraded

--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -1,17 +1,31 @@
 import React from 'react'
-import { Card, ListGroup, ListGroupItem } from 'react-bootstrap'
+import { Card, ListGroup, ListGroupItem, Button } from 'react-bootstrap'
+import { LinkButton } from '.'
 
 import './Vehicles.css'
 
-export default function Vehicles({ vehiclesList }) {
+export default function Vehicles({ vehicles }) {
     return (
         <Card className="listCard">
             <ListGroup>
-                {vehiclesList ?
-                    vehiclesList.vehicles.map(vehicle =>
-                        <ListGroupItem className={"vehicle"} key={vehicle.id}>
-                            <p>{vehicle.brand}</p>
-                            <p>{vehicle.model}</p>
+                {vehicles ?
+                    vehicles.map(vehicle =>
+                        <ListGroupItem className="vehicle" key={vehicle.id}>
+                            <div>
+                                <p>{vehicle.brand}</p>
+                                <p>{vehicle.model}</p>
+                            </div>
+                            <div>
+                                <LinkButton 
+                                    size="sm"
+                                    to='/editcar'
+                                    content='Editar'
+                                    className="listItemBtn"
+                                />
+                                <Button size="sm" className="listItemBtn">
+                                    Eliminar
+                                </Button>
+                            </div>
                         </ListGroupItem>)
                 :
                     <p>Cargando listado...</p>
--- src/screens/ListCar.js
+++ src/screens/ListCar.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { Header, Vehicles, Footer, Input } from '../components'
+import React, { useState, useEffect } from 'react';
+import { Header, Vehicles, Footer, Input } from '../components';
+import axios from 'axios'
 
 import './ListCar.css'
 
@@ -8,24 +8,22 @@ export default function ListCar() {
     const [ vehicles , setVehicles ] = useState(null)
 
     useEffect(() => {
-        // fetch('https://rent-a-car-uade.herokuapp.com/vehicles')
-        // .then( response => {
-        //     console.log(response)
-        //     if(response.ok){
-        //         response.json()
-        //         .then( vehiclesResponse => {
-        //             setVehicles(vehiclesResponse)
-        //         })
-        //         .catch( error => alert("Un error ha ocurrido:", error))
-        //     }
-        // } )
+        axios.get('https://rent-a-car-uade.herokuapp.com/vehicles')
+        .then( 
+            res => {
+                setVehicles(res.data.vehicles)
+            })
+            .catch( error => alert("Un error ha ocurrido", error))
+
     }, [])    
 
     return (
         <div className="list">
             <Header title="Lista de autos" />
-            <Input />
-            <Vehicles vehiclesList={vehicles}/>
+            <Input 
+                className="search"
+                placeholder="Buscar..." />
+            <Vehicles vehicles={vehicles}/>
             <Footer />
             {/* <Link to="/editcar"> Editar auto </Link> */}
         </div>
